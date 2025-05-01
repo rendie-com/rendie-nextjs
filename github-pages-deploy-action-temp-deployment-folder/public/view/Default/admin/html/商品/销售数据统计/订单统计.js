1bc71a43b26104cef0d20321c071e79681ab4a25
rendie <121609433+rendie-com@users.noreply.github.com>
2025-05-01 08:39:14 +0800

    Delete github-pages-deploy-action-temp-deployment-folder directory

--- github-pages-deploy-action-temp-deployment-folder/public/view/Default/admin/html/商品/销售数据统计/订单统计.js
+++
@@ -1,7 +0,0 @@
-let SumMoneyTotal,SumProfit
-function MoneyTotal(m){SumMoneyTotal=m;return m;}//实收金额
-function Profit(m){SumProfit=m;return m;}//利润
-function ProfitMargin(m)//利润率
-{
-	return ((SumProfit/SumMoneyTotal)*100).toFixed(2)+"%"
-}
--- github-pages-deploy-action-temp-deployment-folder/public/view/Default/admin/js/敦煌网/类目/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-!function () {
-    obj.arr[3] = obj.arr[3] ? obj.arr[3] : "-_-20";//选择JS文件
-    let path = "admin/js/敦煌网/类目/"
-    switch (obj.arr[3]) {
-        case "js01": Tool.scriptArr([path + '产品佣金率.js']); break;        
-        default: Tool.scriptArr([path + '【敦煌网】商品类目.js']); break;
-    }
-}();
