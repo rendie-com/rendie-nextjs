8bbc53f810ce7b326a5ce6a6877b0715f5131cc6
rendie <121609433+rendie-com@users.noreply.github.com>
2025-09-29 10:50:50 +0800

    Add files via upload

--- public/view/Default/admin/js/Shopee/商业分析/index.js
+++ public/view/Default/admin/js/Shopee/商业分析/index.js
@@ -1,9 +1,9 @@
 'use strict';
 !function () {
-    //obj.params.jsFile     选择JS文件  
+    //o.params.jsFile     选择JS文件  
     let path = "admin/js/Shopee/商业分析/"
     let loginPath = ["admin/js/Shopee/common.js", "admin/js/Shopee/common_login.js", "admin/js/Shopee/common_登录.js"]
-    switch (obj.params.jsFile) {
+    switch (o.params.jsFile) {
         case "js01": Tool.scriptArr([path + 'common.js', path + '营销/index.js']); break;
         case "js02": Tool.scriptArr([path + 'common.js', path + '相似商品/index.js']); break;
         case "js03": Tool.scriptArr(loginPath.concat([path + '相似商品/获取相似商品.js'])); break;
