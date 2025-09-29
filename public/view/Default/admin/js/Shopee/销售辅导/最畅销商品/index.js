d69ef72c3039a5779b5fc9631151c5bad01740bc
rendie <121609433+rendie-com@users.noreply.github.com>
2025-09-29 10:51:41 +0800

    Add files via upload

--- public/view/Default/admin/js/Shopee/销售辅导/最畅销商品/index.js
+++ public/view/Default/admin/js/Shopee/销售辅导/最畅销商品/index.js
@@ -6,7 +6,7 @@ var fun =
         this.a02();
     },
     a02: function () {
-        let html = Tool.header(obj.params.jsFile)+'\
+        let html = Tool.header(o.params.jsFile)+'\
 		<div class="p-2">\
 			<table class="table align-top table-hover">\
 				<tbody><tr><td>未开发1111111111</td></tr></tbody>\
