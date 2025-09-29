1449982182918734e27b30f077eea967e52a36a1
rendie <121609433+rendie-com@users.noreply.github.com>
2025-09-29 13:24:42 +0800

    Add files via upload

--- public/view/Default/admin/js/Shopee/任务/定时任务/启动定时任务/所有任务/设置商品-置顶推广.js
+++ public/view/Default/admin/js/Shopee/任务/定时任务/启动定时任务/所有任务/设置商品-置顶推广.js
@@ -14,9 +14,12 @@ var task = {
             siteNum: Tool.siteNum(site, num),
         }
         $("#tbody").html('<tr><td class="right">置顶推广进度：</td>' + Tool.htmlProgress('D') + '</tr>')
-        Tool.download_sqlite.a01(["shopee/商品/店铺商品/" + oo.siteNum], this.a02, this, oo)
+        this.a02(oo)
     },
-    a02: function (t, oo) {
+    a02: function (oo) {
+        Tool.download_sqlite.a01(["shopee/商品/店铺商品/" + oo.siteNum], this.a03, this, oo)
+    },
+    a03: function (t, oo) {
         $("#state").html("正在获取商品信息。。。");
         let data = [{
             action: "sqlite",
@@ -30,20 +33,21 @@ var task = {
                 sql: "select count(1) as total FROM @.table where @.status=1",
             })
         }
-        Tool.ajax.a01(data, this.a03, this, oo);
+        Tool.ajax.a01(data, this.a04, this, oo);
     },
-    a03: function (t, oo) {
+    a04: function (t, oo) {
         if (this.obj.D2 == 0) { this.obj.D2 = t[1][0].total }
         if (t[0][0]) {
             oo.fromid = t[0][0].fromid
-            Tool.x1x2("D", this.obj.D1, this.obj.D2, this.a04, this, this.d01, oo)
+            Tool.x1x2("D", this.obj.D1, this.obj.D2, this.d01, this, this.d04, oo)
         }
         else {
             $("#state").html("没有上架的商品。跳过。");
-            this.d01(oo)
+            this.d04(oo)
         }
     },
-    a04: function (oo) {
+    ////////////////////////////////
+    d01: function (oo) {
         let pArr = [
             "version=3.1.0",
             "SPC_CDS=" + oo.seller.SPC_CDS,
@@ -54,44 +58,42 @@ var task = {
         let url = "https://seller.shopee.cn/api/v3/product/boost_product/?" + pArr.join("&")
         let data = '{"id":' + oo.fromid + '}'
         $("#state").html("正在置顶推广。。。");
-        gg.postFetch(url, data, this.a05, this, oo)
+        gg.postFetch(url, data, this.d02, this, oo)
     },
-    a05: function (t, oo) {
+    d02: function (t, oo) {
         if (t.message == "success") {
             $("#state").html("已置顶推广。");
-            this.a06(oo)
+            this.d03(oo)
         }
         else if (t.code == 1000100216) {
             $("#state").html("已经【置顶推广】过了。");
-            this.a06(oo)
+            this.d03(oo)
         }
         else if (t.message == "reached shop's bump slot limit") {
             $("#state").html("已达到商店的凹凸槽限制。");
-            this.d01(oo)
+            this.d04(oo)
         }
         else if (t.code == 1000100217) {
             $("#state").html("置顶完成。");
-            this.d01(oo)
+            this.d04(oo)
         }
         else if (t.message == "item status not normal or stock is 0") {
             //item status not normal or stock is 0
             $("#state").html("物料状态不正常或库存为");
-            this.a06(oo)
+            this.d03(oo)
         }
         else {
             Tool.pre(["出错222：", t])
         }
     },
-    a06: function (oo) {
+    d03: function (oo) {
         $("#state").html("置顶推广完成。。。");
         this.obj.D1++;
         this.a02(oo);
     },
-    d01: function (oo) {
+    d04: function (oo) {
         this.obj.D1 = 1; this.obj.D2 = 0;
         $("#tbody").html("");
         oo.next.apply(oo.This, [oo.t]);
     }
 }
-
-
